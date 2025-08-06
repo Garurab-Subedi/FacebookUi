88a386ab131a54d684867ac717e075eaf962ffd0
Garurab-Subedi <gauaravsubedi299@gmail.com>
2025-08-06 21:48:38 +0545

    first commit

---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Topbar.css'
+import { Search, Person, Message, Notifications } from '@mui/icons-material'
+import profile from '../../assets/person/1.jpeg'
+
+const Topbar = () => {
+  return (
+    <div className="topbarContainer">
+        <div className="topbarLeft">
+            <span className="logo">Facebook</span>
+        </div>
+        <div className="topbarCenter">
+            <div className="searchbar">
+                <Search />
+                <input placeholder="Search for friends, posts or videos" className="searchInput" />
+            </div>
+        </div>
+        <div className="topbarRight">
+            <div className="topbarLinks">
+                <span className="topbarLink">Homepage</span>
+                <span className="topbarLink">Timeline</span>
+            </div>
+            <div className="topbarIcons">
+                <div className="topbarIconItem">
+                    <Person />
+                    <span className="topbarIconBadge">1</span>
+                </div>
+                <div className="topbarIconItem">
+                    <Message />
+                    <span className="topbarIconBadge">2</span>
+                </div>
+                <div className="topbarIconItem">
+                    <Notifications />
+                    <span className="topbarIconBadge">1</span>
+                </div>
+            </div>
+            <img src={profile} alt="" className="topbarImg" height="50px"/>
+        </div>
+    </div>
+  )
+}
+
+export default Topbar
