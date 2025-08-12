099e14d6d0f62a0711820e2b1f80bd58e76e399d
Garurab-Subedi <gauaravsubedi299@gmail.com>
2025-08-12 16:47:19 +0545

    UI: Login page UI has done

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Home from './pages/home/Home';
 import './App.css';
 import Profile from './pages/profile/Profile';
+import Login from './pages/login/Login';
 
 function App() {
 
   return (
     <div>
-      <Profile />
+      <Login />
     </div>
   )
 }
